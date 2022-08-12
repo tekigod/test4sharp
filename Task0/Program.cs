cf1dcf249ba94b872ba8d454d2457de71992ad58
bladeshape <tekigod@mail.ru>
2022-08-12 05:30:34 +0300

    code1

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
